80e6d316b978786edaf8328d7ce19fa78426c79f
Tirth <tirthacodes@gmail.com>
2025-05-22 10:57:54 +0545

    created actor model

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int id { get; set; }
+        public string profilePictureURL { get; set; }
+        public string fullName { get; set; }
+        public string bio { get; set; }
+    }
+}
