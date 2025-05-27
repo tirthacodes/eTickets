e01e1ba3e30fdbaaa733397150c61617813a7d33
Tirth <tirthacodes@gmail.com>
2025-05-27 14:37:07 +0545

    create actor form

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -19,5 +19,12 @@ namespace eTickets.Controllers
             var data = await _service.GetAll();
             return View(data);
         }
+
+        //Get: Actors/Create
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
