f9b881d5b1894ed1be61add3cd703022a8bc5598
Tirth <tirthacodes@gmail.com>
2025-05-29 00:14:28 +0545

    getallasync and getbyidasync implemented

---
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,43 @@
+﻿
+using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        private readonly AppDbContext _context;
+        public EntityBaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            var result = await _context.Set<T>().ToListAsync();
+            return result;
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var result = await _context.Set<T>().FirstOrDefaultAsync(n => n.id == id);
+            return result;
+        }
+
+        public Task<T> UpdateAsync(int id, T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
